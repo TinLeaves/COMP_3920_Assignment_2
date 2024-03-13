e001a45cca73a1d883f2b257d81b4cf8e400fbaf
TinLeaves <tinleaves.0@gmail.com>
2024-03-13 12:06:42 -0700

    configured to use mysql as database

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require("./utils.js");
+require("./utils");
 
 require('dotenv').config();
 const express = require('express');
@@ -6,6 +6,11 @@ const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const bcrypt = require('bcrypt');
 const saltRounds = 12;
+
+const database = include('databaseConnection');
+const db_utils = include('database/db_utils');
+const db_users = include('database/db_users');
+const success = db_utils.printMySQLVersion();
 
 const port = process.env.PORT || 3020;
 
@@ -23,20 +28,10 @@ const expireTime = 1 * 60 * 60 * 1000; //expires after 1 hour  (hours * minutes 
 const mongodb_host = process.env.MONGODB_HOST;
 const mongodb_user = process.env.MONGODB_USER;
 const mongodb_password = process.env.MONGODB_PASSWORD;
-const mongodb_database = process.env.MONGODB_DATABASE;
 const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
 
 const node_session_secret = process.env.NODE_SESSION_SECRET;
 /* END secret section */
-
-// var { database } = include('databaseConnection');
-const { database } = require('./databaseConnection');
-
-
-const userCollection = database.db(mongodb_database).collection('users');
-
-// const { ObjectId } = require('mongodb');
-const { MongoClient, ObjectId } = require('mongodb');
 
 app.use(express.urlencoded({ extended: false }));
 
@@ -71,42 +66,11 @@ function sessionValidation(req, res, next) {
   }
 }
 
-function isAdmin(req) {
-  if (req.session.user_type == 'admin') {
-    return true;
-  }
-  return false;
-}
-
-function adminAuthorization(req, res, next) {
-  if (!isAdmin(req)) {
-    res.status(403);
-    res.render("403", { error: "Not Authorized" });
-    return;
-  }
-  else {
-    next();
-  }
-}
-
 app.get('/', (req, res) => {
   const authenticated = req.session.authenticated;
   const username = req.session.username;
 
   res.render('index', { authenticated, username });
-});
-
-app.get('/members', (req, res) => {
-  if (!req.session.authenticated) {
-    res.redirect('/login');
-    return;
-  }
-
-  const images = ['atSad.gif', 'atVibe.gif', 'atSoupMe.gif'];
-  const randomIndex = Math.floor(Math.random() * images.length);
-  const randomImage = '/public/' + images[randomIndex];
-
-  res.render('members', { username: req.session.username, randomImage });
 });
 
 app.get('/logout', (req, res) => {
@@ -123,29 +87,30 @@ app.post('/signupSubmit', async (req, res) => {
   var email = req.body.email;
   var password = req.body.password;
 
-// Check for empty fields
-let errorMsg = "";
-if (!username) {
-  errorMsg += "Name is required.";
-}
-if (!email) {
-  errorMsg += "Email is required.";
-}
-if (!password) {
-  errorMsg += "Password is required.";
-}
-if (errorMsg !== "") {
-  res.render('signupSubmit', { errorMsg: errorMsg });
-  return;
-}
+  // Check for empty fields
+  let errorMsg = "";
+  if (!username) {
+    errorMsg += "Name is required.";
+  }
+  if (!email) {
+    errorMsg += "Email is required.";
+  }
+  if (!password) {
+    errorMsg += "Password is required.";
+  }
+  if (errorMsg !== "") {
+    res.redirect(`/signup?error=${encodeURIComponent(errorMsg)}`);
+    return;
+  }
 
   // Validate inputs using Joi
-  const schema = Joi.object(
-    {
-      username: Joi.string().alphanum().max(20).required(),
-      email: Joi.string().email().required(),
-      password: Joi.string().max(20).required()
-    });
+  const schema = Joi.object({
+    username: Joi.string().alphanum().max(20).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string()
+      .pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{10,}$'))
+      .required()
+  });
 
   const validationResult = schema.validate({ username, email, password });
   if (validationResult.error != null) {
@@ -154,25 +119,29 @@ if (errorMsg !== "") {
     return;
   }
 
-  // Check if username already exists
-  const existingUser = await userCollection.findOne({ username: { $eq: username } });
+  // Check if username already exists in MySQL database
+  const existingUser = await db_users.getUser({ user: username });
   if (existingUser) {
     const errorMsg = "Username already exists.";
     res.render('signupSubmit', { errorMsg: errorMsg });
     return;
   }
 
+  // Hash password
   var hashedPassword = await bcrypt.hash(password, saltRounds);
 
-  // Add user to MongoDB database using parameterized query
-  await userCollection.insertOne({
-    username,
-    email,
-    password: hashedPassword,
-    user_type: "user", // Assigning a default user_type of "user"
+  // Insert user into MySQL database
+  const createUserResult = await db_users.createUser({
+    user: username,
+    hashedPassword: hashedPassword,
+    email: email
   });
 
-  console.log("Inserted user");
+  if (!createUserResult) {
+    console.log("Error inserting user into MySQL database");
+    res.redirect("/signup");
+    return;
+  }
 
   // Set session variables
   req.session.authenticated = true;
@@ -183,84 +152,52 @@ if (errorMsg !== "") {
   res.redirect("/members");
 });
 
+
 app.get('/login', (req, res) => {
   res.render('login');
 });
 
 app.post('/loginSubmit', async (req, res) => {
-  // extract email and password from request body
-  const { email, password } = req.body;
+  // extract username and password from request body
+  const { username, password } = req.body;
 
-  // validate email using Joi
-  const emailSchema = Joi.string().email().required();
-  const emailValidationResult = emailSchema.validate(email);
-  if (emailValidationResult.error != null) {
-    console.log(emailValidationResult.error);
+  // Login handler
+  // Validate username
+  const usernameSchema = Joi.string().alphanum().max(20).required();
+  const usernameValidationResult = usernameSchema.validate(username);
+  if (usernameValidationResult.error != null) {
+    console.log(usernameValidationResult.error);
     res.redirect('/login');
     return;
   }
 
-  // find user in database using email
-  const user = await userCollection.findOne({ email });
+  // Find user in database using username
+  const user = await db_users.getUser({ user: username });
+
   if (!user) {
-    console.log('invalid email/password combination');
-    const errorMsg = 'Invalid email/password combination.';
+    console.log('Invalid username/password combination');
+    const errorMsg = 'Invalid username/password combination.';
     res.render('loginSubmit', { errorMsg: errorMsg });
     return;
   }
 
-  // compare password with stored BCrypted password
+  // Compare password with stored BCrypt password
   const isPasswordMatch = await bcrypt.compare(password, user.password);
   if (!isPasswordMatch) {
-    console.log('password is incorrect');
+    console.log('Password is incorrect');
     const errorMsg = 'Password is incorrect.';
     res.render('loginSubmit', { errorMsg: errorMsg });
     return;
   }
 
-  // store username in session
+  // Store username in session
   req.session.authenticated = true;
   req.session.username = user.username;
   req.session.cookie.maxAge = expireTime;
   req.session.user_type = user.user_type;
 
-  // redirect to members page
+  // Redirect to members page
   res.redirect('/members');
-});
-
-app.get('/admin', sessionValidation, adminAuthorization, async (req, res) => {
-  if (!req.session.authenticated) {
-    res.redirect('/login');
-    return;
-  }
-
-  if (!isAdmin(req)) {
-    res.status(403);
-    res.render("403", { error: "Not Authorized" });
-    return;
-  }
-
-  const result = await userCollection.find({}, { projection: { username: 1, user_type: 1 } }).toArray();
-
-  res.render("admin", { users: result });
-});
-
-app.get('/admin/demote', async (req, res) => {
-  var user = req.query.user;
-
-  // Update user'a user_type using parameterized query
-  await userCollection.updateOne({ username: { $eq: user } }, { $set: { user_type: "user" } });
-
-  res.redirect("/admin");
-});
-
-app.get('/admin/promote', async (req, res) => {
-  var user = req.query.user;
-
-  // Update user's user_type using parameterized query
-  await userCollection.updateOne({ username: { $eq: user } }, { $set: { user_type: "admin" } });
-
-  res.redirect("/admin");
 });
 
 app.use(express.static(__dirname + "/public"));
